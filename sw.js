9a27ef549c1bdae628a92884d613f819e1d84c69
heri917 <arnstorecell@gmail.com>
2026-09-14 11:04:34 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const C="arn-v2";
-const A=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./app-icon.png","./header-logo.png"];
+const A=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./app-icon-192.png","./header-logo.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
